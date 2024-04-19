0a3216efd2b8bddae2376298c4bd51e23ab5bd91
Italo <itallo016@gmail.com>
2024-04-19 00:03:01 -0300

    Inicio Aula 2

---
+++ Aula02/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aula02{
+    class Program{
+        static void Main(String[] args){
+            Console.WriteLine("Oiiiii");
+            Console.ReadLine();
+        }
+    }
+}
