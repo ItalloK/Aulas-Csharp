4048a14be3fe94b72c4b3c40952ce752eb215dd3
Italo <itallo016@gmail.com>
2024-04-20 22:01:10 -0300

    Aula 09

--- Aula09/Aula09.cs
+++ Aula09/Aula09.cs
@@ -3,17 +3,13 @@ using Internal;
 
 class Aula09{
     static void Main(){
-
+        
         /*Operadores Bitwise: 
             ( >> ) -> Dobra o valor da Variavel
             ( << ) -> Metade do valor da Variavel
         */
-
         int Num = 2;
         Num = Num << 2;
-
         Console.WriteLine("Valor: {0}", Num);
-
-
     }
 }
