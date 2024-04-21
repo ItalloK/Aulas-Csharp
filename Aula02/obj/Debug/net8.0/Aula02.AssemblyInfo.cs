70a73a58ea946235c54241e4fcc7c518efd91440
Italo <itallo016@gmail.com>
2024-04-21 13:30:24 -0300

    Calculadora de Media

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00fb0f8d65fc7e6c9c3afb525be49d2d82e3e1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfac1fd57633d40e58f69e6711fa6bb251296b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
