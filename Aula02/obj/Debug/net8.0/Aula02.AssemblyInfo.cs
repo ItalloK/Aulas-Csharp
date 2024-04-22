97d3346218b7d583d1a29c0eee751630bfb9cad7
Italo <itallo016@gmail.com>
2024-04-21 21:51:18 -0300

    Aula 14

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a73a58ea946235c54241e4fcc7c518efd91440")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2cba5d2e1b1fab16b0df68e6d83f6284e1bd66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
