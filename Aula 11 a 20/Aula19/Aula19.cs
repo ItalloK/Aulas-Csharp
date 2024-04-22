e39d5a4fcd34b9711a67067232d0721ece8d7315
Italo <itallo016@gmail.com>
2024-04-22 13:37:39 -0300

    Aula 19

--- Aula 11 a 20/Aula19/Aula19.cs
+++ Aula 11 a 20/Aula19/Aula19.cs
@@ -4,10 +4,13 @@ using Internal;
 class Aula19{
     static void Main(){
         int[] Num = new int [10];
+
         int[,] Matriz = new int [5,5];
-        for(int i=0; i<10; i++){
+
+        for(int i=0; i<Num.Length; i++){ // Num.Length -> Retorna o tamanho em INT do vetor.
             Num[i] = 15;
         }
+
         for(int i=0; i<10; i++){
             Console.WriteLine(Num[i]);
         }
